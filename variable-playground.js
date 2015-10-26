cbc21c3a8e07d07d4922f3c88bf3d35123b5bdfe
Fang Yang <53211yf@gmail.com>
2015-10-26 14:43:20 -0700

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+ // var person = {
+ // 	name: "Andrew",
+ // 	age: 21
+ // };
+
+ // function updatePerson (obj) {
+ // 	// obj = {
+ // 	// 	name: "Andrew",
+ // 	// 	age: 24
+ // 	// };
+
+ // 	obj.age = 24;
+ // }
+
+ // updatePerson(person);
+ // console.log(person);
+
+ var grades = [15, 88];
+
+ function addGrads (gradesArr) {
+ 	gradesArr.push(55);
+ 	debugger;
+ }
+
+ addGrads(grades);
+ console.log(grades);
