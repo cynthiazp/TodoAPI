200c43befc49c76a326b4c7e357a71cae874cf74
Fang Yang <53211yf@gmail.com>
2015-10-27 16:55:55 -0700

    Add user model and POST /users request

--- db.js
+++ db.js
@@ -13,14 +13,11 @@ else {
 		"storage": __dirname + "/data/dev-todo-api.sqlite"
 	});
 }
-// var sequelize = new Sequelize(undefined, undefined, undefined, {
-// 	"dialect": "sqlite",
-// 	"storage": __dirname + "/data/dev-todo-api.sqlite"
-// });
 
 var db = {};
 
 db.todo = sequelize.import(__dirname + "/models/todo.js");
+db.user = sequelize.import(__dirname + "/models/user.js");
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
